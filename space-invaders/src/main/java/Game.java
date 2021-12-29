d125cb00d3c6b58b26903534c02367ac35cbcde5
Tiago Branquinho <tiagobranquinho@MacBook-Air-de-Tiago.local>
2021-12-29 15:55:02 +0000

    Added application class

--- space-invaders/src/main/java/Game.java
+++ space-invaders/src/main/java/Game.java
@@ -1,2 +1,39 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Game {
+    private Screen screen;
+    private final int width = 40;
+    private final int height = 20;
+
+    public Game() {
+        try {
+            TerminalSize terminalSize = new TerminalSize(width, height);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            this.screen = screen;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.refresh();
+    }
+
+    public void run(){
+
+    }
 }
