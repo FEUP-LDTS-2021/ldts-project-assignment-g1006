fef19bbafb075675058a8ab3b8dc46697103dd36
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-02 14:50:06 +0000

    Added Position class

---
+++ space-invaders/src/main/java/Position.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        setX(x);
+        setY(y);
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
