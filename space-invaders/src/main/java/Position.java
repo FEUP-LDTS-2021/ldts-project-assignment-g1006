c5925fd9ae321db8b67497d525d628d0cc1e93fe
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-02 14:54:36 +0000

    Added equals and hashcode Position methods

--- space-invaders/src/main/java/Position.java
+++ space-invaders/src/main/java/Position.java
@@ -24,4 +24,17 @@ public class Position {
     public void setY(int y) {
         this.y = y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return getX() == position.getX() && getY() == position.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
 }
