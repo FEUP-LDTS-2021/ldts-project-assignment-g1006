b0fa83895cbecd92818db80c1fed700d2c5b6dfa
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-03 23:26:47 +0000

    Added Ammo class

---
+++ src/main/java/Ammo.java
@@ -0,0 +1,41 @@
+public class Ammo extends Element {
+    private enum directions{UP, DOWN}
+    private int direction;
+    private int damage;
+
+    public Ammo(int x, int y, int direction, int damage) {
+        super(x, y);
+        this.direction = direction;
+        this.damage = damage;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        super.setPosition(position);
+    }
+
+    @Override
+    public Position getPosition() {
+        return super.getPosition();
+    }
+
+    public void shoot(){
+
+    }
+}
