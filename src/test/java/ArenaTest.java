43c3225aba9c881d1eb5d568268ea7bf2c2e6e3c
Tiago Branquinho <tiagobranquinho@Air-de-Tiago.lan>
2022-01-04 18:47:34 +0000

    Testing Element Draw method

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -13,8 +13,51 @@ public class ArenaTest {
         Arena arena = new Arena(10,10);
         arena.setProjectiles(List.of(ammo));
 
+        Player player = Mockito.mock(Player.class);
+        arena.setPlayer(player);
+        Alien alien = Mockito.mock(Alien.class);
+        arena.setAliens(List.of(alien));
+
+
         arena.draw(screen.newTextGraphics());
 
         Mockito.verify(ammo, Mockito.times(1)).draw(screen.newTextGraphics());
     }
+
+    @Test
+    public void drawPlayer(){
+        Screen screen = Mockito.mock(Screen.class);
+        Player player = Mockito.mock(Player.class);
+        Arena arena = new Arena(10,10);
+        arena.setPlayer(player);
+
+        Ammo ammo = Mockito.mock(Ammo.class);
+        arena.setProjectiles(List.of(ammo));
+        Alien alien = Mockito.mock(Alien.class);
+        arena.setAliens(List.of(alien));
+
+        arena.draw(screen.newTextGraphics());
+
+        Mockito.verify(player, Mockito.times(1)).draw(screen.newTextGraphics());
+    }
+
+    @Test
+    public void drawAlien(){
+        Screen screen = Mockito.mock(Screen.class);
+        Player player = Mockito.mock(Player.class);
+        Arena arena = new Arena(10,10);
+        arena.setPlayer(player);
+
+        Ammo ammo = Mockito.mock(Ammo.class);
+        arena.setProjectiles(List.of(ammo));
+        Alien alien = Mockito.mock(Alien.class);
+        arena.setAliens(List.of(alien));
+
+
+
+        arena.draw(screen.newTextGraphics());
+
+        Mockito.verify(player, Mockito.times(1)).draw(screen.newTextGraphics());
+    }
+
 }
