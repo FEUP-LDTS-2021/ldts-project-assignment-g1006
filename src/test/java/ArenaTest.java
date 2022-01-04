40b3f0b0d38ab97889193c73a33fb2576a24d76a
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-04 01:55:24 +0000

    Added Arena tests

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ArenaTest {
+import com.googlecode.lanterna.screen.Screen;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class ArenaTest {
+
+    @Test
+    public void drawAmmo(){
+        Screen screen = Mockito.mock(Screen.class);
+        Ammo ammo = Mockito.mock(Ammo.class);
+        Arena arena = new Arena(10,10);
+        arena.setProjectiles(List.of(ammo));
+
+        arena.draw(screen.newTextGraphics());
+
+        Mockito.verify(ammo, Mockito.times(1)).draw(screen.newTextGraphics());
+    }
 }
