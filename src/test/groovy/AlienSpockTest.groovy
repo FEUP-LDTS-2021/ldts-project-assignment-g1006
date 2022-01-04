fe039ebd55846af69f245a9f19cdd9965b6acdce
unknown <henri11silvafcc@gmail.com>
2022-01-04 21:07:01 +0000

    Added Alien Drawing Tests

--- src/test/groovy/AlienSpockTest.groovy
+++ src/test/groovy/AlienSpockTest.groovy
@@ -1,6 +1,14 @@
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
+import org.mockito.Mockito
 import spock.lang.Specification
 
 class AlienSpockTest extends Specification{
+    private Screen screen
+
     def "alien movement"(){
         given:
         def alien = new Alien(10,20,'A' as char)
@@ -14,6 +22,17 @@ class AlienSpockTest extends Specification{
         position1.getY() == 20
         position2.getX() == 11
         position2.getY() == 20
+    }
+
+    def "draw alien character"(){
+        given:
+        def alien = new Alien(10,10, 'A' as char)
+
+        when:
+        alien.draw(screen.newTextGraphics())
+
+        then:
+        screen.getBackCharacter(10,10).getCharacter() == alien.getCharacter()
     }
 }
 
