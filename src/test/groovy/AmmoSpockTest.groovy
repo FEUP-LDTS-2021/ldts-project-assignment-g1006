2b7814b2b83ab5099fcb1d53a400b34437d49b11
unknown <henri11silvafcc@gmail.com>
2022-01-04 21:17:54 +0000

    Added Ammo Drawing Tests

---
+++ src/test/groovy/AmmoSpockTest.groovy
@@ -0,0 +1,30 @@
+import com.googlecode.lanterna.TerminalSize
+import com.googlecode.lanterna.screen.Screen
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.Terminal
+import org.mockito.Mockito
+import spock.lang.Specification
+
+class AmmoSpockTest extends Specification{
+    private Screen screen
+
+    def setup(){
+        def terminalSize = new TerminalSize(20, 20)
+        def terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize)
+        def terminal = terminalFactory.createTerminal()
+        Screen screen = new TerminalScreen(terminal)
+        this.screen = screen
+    }
+
+    def "draw ammo character"(){
+        given:
+        def ammo = new Ammo(10,10, 'B' as char, 0, 0)
+
+        when:
+        ammo.draw(screen.newTextGraphics())
+
+        then:
+        screen.getBackCharacter(10,10).getCharacter() == ammo.getCharacter()
+    }
+}
