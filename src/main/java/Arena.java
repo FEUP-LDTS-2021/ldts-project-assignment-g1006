4210727a754690ee4687f0591bccb893f5f3b110
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-04 00:07:26 +0000

    Added Arena class

---
+++ src/main/java/Arena.java
@@ -0,0 +1,47 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+import java.util.List;
+
+public class Arena {
+    private int width;
+    private int height;
+    private Player player;
+    private List<Alien> aliens;
+    private List<Ammo> projectiles;
+
+    public Arena(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public List<Alien> getAliens() {
+        return aliens;
+    }
+
+    public void setAliens(List<Alien> aliens) {
+        this.aliens = aliens;
+    }
+
+    public List<Ammo> getProjectiles() {
+        return projectiles;
+    }
+
+    public void setProjectiles(List<Ammo> projectiles) {
+        this.projectiles = projectiles;
+    }
+
+    public void draw(TextGraphics screen){
+
+    }
+
+    public boolean checkProjectilesCollision(){
+        return false;
+    }
+}
