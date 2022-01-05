fba78b65621b96cbf409d74a37a8c423eb8ffbe2
unknown <henri11silvafcc@gmail.com>
2022-01-05 20:16:37 +0000

    Check Alien Projectiles Collisions Tests

--- src/test/groovy/ArenaSpockTest.groovy
+++ src/test/groovy/ArenaSpockTest.groovy
@@ -19,4 +19,22 @@ class ArenaSpockTest extends Specification{
         then:
         arena.checkAlienLimitsCollisions()
     }
+
+    def "alien and projectiles collisions"(){
+        given:
+        def arena = new Arena(40, 20)
+        def alien = new Alien(10,4,'A' as char)
+        def ammo = new Ammo(10, 4, 'B' as char, -1, 1)
+
+        when:
+        def aliens = [alien]
+        def projectiles = [ammo]
+        arena.setAliens(aliens)
+        arena.setProjectiles(projectiles)
+
+        then:
+        arena.checkAlienProjectilesCollisions()
+        !aliens.contains(alien)
+        !projectiles.contains(ammo)
+    }
 }
