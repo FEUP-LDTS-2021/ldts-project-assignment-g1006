da2f18bb4deaa876200cfb75a43df9ba30759b7c
Tiago Branquinho <tiagobranquinho@Air-de-Tiago.lan>
2022-01-05 22:48:03 +0000

    Arena testing update

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -1,62 +1,44 @@
 import com.googlecode.lanterna.screen.Screen;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
 
 public class ArenaTest {
+    private Arena arena;
+    private Screen screen;
+    private Ammo ammo;
+    private Player player;
+    private Alien alien;
+
+    @BeforeEach
+    public void setup(){
+        screen = Mockito.mock(Screen.class);
+        arena = new Arena(10,10);
+        ammo = Mockito.mock(Ammo.class);
+        arena.setProjectiles(List.of(ammo));
+        alien = Mockito.mock(Alien.class);
+        arena.setAliens(List.of(alien));
+        player = Mockito.mock(Player.class);
+        arena.setPlayer(player);
+    }
 
     @Test
     public void drawAmmo(){
-        Screen screen = Mockito.mock(Screen.class);
-        Ammo ammo = Mockito.mock(Ammo.class);
-        Arena arena = new Arena(10,10);
-        arena.setProjectiles(List.of(ammo));
-
-        Player player = Mockito.mock(Player.class);
-        arena.setPlayer(player);
-        Alien alien = Mockito.mock(Alien.class);
-        arena.setAliens(List.of(alien));
-
-
         arena.draw(screen.newTextGraphics());
-
         Mockito.verify(ammo, Mockito.times(1)).draw(screen.newTextGraphics());
     }
 
     @Test
     public void drawPlayer(){
-        Screen screen = Mockito.mock(Screen.class);
-        Player player = Mockito.mock(Player.class);
-        Arena arena = new Arena(10,10);
-        arena.setPlayer(player);
-
-        Ammo ammo = Mockito.mock(Ammo.class);
-        arena.setProjectiles(List.of(ammo));
-        Alien alien = Mockito.mock(Alien.class);
-        arena.setAliens(List.of(alien));
-
         arena.draw(screen.newTextGraphics());
-
         Mockito.verify(player, Mockito.times(1)).draw(screen.newTextGraphics());
     }
 
     @Test
     public void drawAlien(){
-        Screen screen = Mockito.mock(Screen.class);
-        Player player = Mockito.mock(Player.class);
-        Arena arena = new Arena(10,10);
-        arena.setPlayer(player);
-
-        Ammo ammo = Mockito.mock(Ammo.class);
-        arena.setProjectiles(List.of(ammo));
-        Alien alien = Mockito.mock(Alien.class);
-        arena.setAliens(List.of(alien));
-
-
-
         arena.draw(screen.newTextGraphics());
-
         Mockito.verify(player, Mockito.times(1)).draw(screen.newTextGraphics());
     }
 }
