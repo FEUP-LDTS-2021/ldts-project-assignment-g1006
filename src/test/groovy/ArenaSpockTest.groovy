91daba9fe481f6b27cdbbd7eb5b204404dda3523
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-06 11:54:35 +0000

    Removed alien and arena limits collision test

--- src/test/groovy/ArenaSpockTest.groovy
+++ src/test/groovy/ArenaSpockTest.groovy
@@ -7,18 +7,6 @@ import org.mockito.Mockito
 import spock.lang.Specification
 
 class ArenaSpockTest extends Specification{
-    def "alien and walls collisions"(){
-        given:
-        def arena = new Arena(40, 20)
-        def alien = new Alien(40,4,'A' as char)
-
-        when:
-        def aliens = [alien]
-        arena.setAliens(aliens)
-
-        then:
-        arena.checkAlienLimitsCollisions()
-    }
 
     def "alien and projectiles collisions"(){
         given:
