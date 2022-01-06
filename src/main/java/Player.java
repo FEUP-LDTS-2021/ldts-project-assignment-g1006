a8e44d859ee03a9d2fa38f77b39efe870250bd24
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-06 15:38:02 +0000

    Updated player move methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -17,12 +17,12 @@ public class Player extends Element{
     }
 
     public void moveLeft(Arena arena){
-        if (getPosition().getX() >= 0)
+        if (getPosition().getX() > 0)
             setPosition(new Position(getPosition().getX() - 1, getPosition().getY()));
     }
 
     public void moveRight(Arena arena) {
-        if (getPosition().getX() <= arena.getWidth()-1)
+        if (getPosition().getX() < arena.getWidth()-1)
             setPosition(new Position(getPosition().getX() + 1, getPosition().getY()));
     }
 
