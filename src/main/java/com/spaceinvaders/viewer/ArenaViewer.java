dcefb13a7baa14c9091f73f54239d5526580f3a3
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-15 19:17:26 +0000

    Added ArenaViewer interface

---
+++ src/main/java/com/spaceinvaders/viewer/ArenaViewer.java
@@ -0,0 +1,18 @@
+package com.spaceinvaders.viewer;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.Arena;
+
+public class ArenaViewer {
+    private final GUI gui;
+    private final Arena arena;
+
+    public ArenaViewer(GUI gui, Arena arena) {
+        this.gui = gui;
+        this.arena = arena;
+    }
+
+    public void draw() {
+
+    }
+}
