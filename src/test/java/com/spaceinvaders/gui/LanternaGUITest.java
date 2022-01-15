a2a9cc64deb7fe8e761652128113b8aadd6ba47b
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-15 10:56:33 +0000

    Added keys/actions test to LanternaGUI tests

--- src/test/java/com/spaceinvaders/gui/LanternaGUITest.java
+++ src/test/java/com/spaceinvaders/gui/LanternaGUITest.java
@@ -2,6 +2,8 @@ package com.spaceinvaders.gui;
 
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.spaceinvaders.Position;
 import org.junit.jupiter.api.Test;
@@ -9,13 +11,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
+import java.io.IOException;
+
 public class LanternaGUITest {
     private LanternaGUI gui;
     private TextGraphics textGraphics;
+    private TerminalScreen terminalScreen;
 
     @BeforeEach
     void setup(){
-        TerminalScreen terminalScreen = Mockito.mock(TerminalScreen.class);
+        this.terminalScreen = Mockito.mock(TerminalScreen.class);
         this.gui = new LanternaGUI(terminalScreen);
         this.textGraphics = Mockito.mock(TextGraphics.class);
         Mockito.when(terminalScreen.newTextGraphics()).thenReturn(textGraphics);
@@ -59,5 +64,44 @@ public class LanternaGUITest {
         gui.drawBackground();
         String color = "#3360FF";
         Mockito.verify(textGraphics, Mockito.times(1)).setBackgroundColor(TextColor.Factory.fromString(color));
+    }
+
+    @Test
+    void getKeyClose() throws IOException {
+        KeyStroke key = Mockito.mock(KeyStroke.class);
+        Mockito.when(terminalScreen.pollInput()).thenReturn(key);
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.Character);
+        Mockito.when(key.getCharacter()).thenReturn('q');
+        Assertions.assertEquals(gui.getAction(), GUI.Action.EXIT);
+
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.EOF);
+        Assertions.assertEquals(gui.getAction(), GUI.Action.EXIT);
+    }
+
+    @Test
+    void getKeyRight() throws IOException {
+        KeyStroke key = Mockito.mock(KeyStroke.class);
+        Mockito.when(terminalScreen.pollInput()).thenReturn(key);
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.ArrowRight);
+
+        Assertions.assertEquals(gui.getAction(), GUI.Action.KEYRIGHT);
+    }
+
+    @Test
+    void getKeyLeft() throws IOException {
+        KeyStroke key = Mockito.mock(KeyStroke.class);
+        Mockito.when(terminalScreen.pollInput()).thenReturn(key);
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.ArrowLeft);
+
+        Assertions.assertEquals(gui.getAction(), GUI.Action.KEYLEFT);
+    }
+
+    @Test
+    void getKeyUp() throws IOException {
+        KeyStroke key = Mockito.mock(KeyStroke.class);
+        Mockito.when(terminalScreen.pollInput()).thenReturn(key);
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.ArrowUp);
+
+        Assertions.assertEquals(gui.getAction(), GUI.Action.KEYUP);
     }
 }
