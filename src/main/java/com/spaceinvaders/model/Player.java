357735fc7b80f3dcd8c7550b2717d325e77fbb35
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 17:21:25 +0000

    Refactoring - Removed unused Player model methods

--- src/main/java/com/spaceinvaders/model/Player.java
+++ src/main/java/com/spaceinvaders/model/Player.java
@@ -15,19 +15,4 @@ public class Player extends Element {
     public Position getPosition() {
         return super.getPosition();
     }
-
-    public void moveLeft(Arena arena){
-        if (getPosition().getX() > 0)
-            setPosition(new Position(getPosition().getX() - 1, getPosition().getY()));
-    }
-
-    public void moveRight(Arena arena) {
-        if (getPosition().getX() < arena.getWidth()-1)
-            setPosition(new Position(getPosition().getX() + 1, getPosition().getY()));
-    }
-
-    public void shoot(Arena arena){
-        Ammo ammo = new Ammo(getPosition().getX(), getPosition().getY() - 1, '|', -1, 1);
-        arena.getProjectiles().add(ammo);
-    }
 }
