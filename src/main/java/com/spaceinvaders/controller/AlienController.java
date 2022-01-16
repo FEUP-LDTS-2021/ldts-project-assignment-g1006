d1e40dba2b61e1ae7c1ec29dc058a789d2815e2b
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 00:45:24 +0000

    Added AlienController interface

---
+++ src/main/java/com/spaceinvaders/controller/AlienController.java
@@ -0,0 +1,20 @@
+package com.spaceinvaders.controller;
+
+import com.spaceinvaders.model.Alien;
+import com.spaceinvaders.model.Arena;
+
+
+public class AlienController extends Controller<Arena>{
+    public AlienController(Arena model) {
+        super(model);
+    }
+
+    @Override
+    public void step() {
+
+    }
+
+    private void move(Alien alien){
+
+    }
+}
