8098a77ac1733180fc43d7ddc8e4021fa5629621
Tiago <t.branquinho.slb@gmail.com>
2022-01-16 19:24:38 +0000

    Normal Alien Strategy handle shot implementation

--- src/main/java/com/spaceinvaders/model/NormalAlienStrategy.java
+++ src/main/java/com/spaceinvaders/model/NormalAlienStrategy.java
@@ -3,6 +3,6 @@ package com.spaceinvaders.model;
 public class NormalAlienStrategy implements AlienStrategy{
     @Override
     public int handleShot(int armor, Ammo ammo) {
-        return 0;
+        return armor - ammo.getDamage();
     }
 }
