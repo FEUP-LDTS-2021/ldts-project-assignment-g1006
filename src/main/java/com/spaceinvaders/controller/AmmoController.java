60b42518680cf7fb759641271bdb19189f2280c1
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 12:48:00 +0000

    Added AmmoController interface

---
+++ src/main/java/com/spaceinvaders/controller/AmmoController.java
@@ -0,0 +1,14 @@
+package com.spaceinvaders.controller;
+
+import com.spaceinvaders.model.Arena;
+
+public class AmmoController extends Controller<Arena>{
+    public AmmoController(Arena model) {
+        super(model);
+    }
+
+    @Override
+    public void step() {
+
+    }
+}
