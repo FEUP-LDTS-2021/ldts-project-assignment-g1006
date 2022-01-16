6ce8b2692d4e905d1bec60b9181d46e98898a527
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 21:51:12 +0000

    Added WallViewer class

---
+++ src/main/java/com/spaceinvaders/viewer/WallViewer.java
@@ -0,0 +1,11 @@
+package com.spaceinvaders.viewer;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.Wall;
+
+public class WallViewer implements ElementViewer<Wall> {
+    @Override
+    public void drawElement(Wall element, GUI gui) {
+
+    }
+}
