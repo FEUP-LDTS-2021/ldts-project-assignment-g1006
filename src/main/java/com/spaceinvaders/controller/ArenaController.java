5d8b9aba94be2f574028ae1d8837e615ab79b2a4
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 12:52:20 +0000

    Updated ArenaController step method

--- src/main/java/com/spaceinvaders/controller/ArenaController.java
+++ src/main/java/com/spaceinvaders/controller/ArenaController.java
@@ -14,23 +14,33 @@ import java.util.List;
 public class ArenaController extends Controller<Arena> {
     private final PlayerController playerController;
     private final AlienController alienController;
+    private final AmmoController ammoController;
     private final ArenaViewer arenaViewer;
     private final GUI gui;
+    private boolean exit;
 
     public ArenaController(Arena model, GUI gui) {
         super(model);
         this.gui = gui;
         this.playerController = new PlayerController(getModel().getPlayer());
         this.alienController = new AlienController(getModel());
+        this.ammoController = new AmmoController(getModel());
         this.arenaViewer = new ArenaViewer(gui, getModel());
+        this.exit = false;
     }
 
-    public ArenaController(Arena arena, ArenaViewer arenaViewer, GUI gui, PlayerController playerController, AlienController alienController) {
+    public ArenaController(Arena arena, ArenaViewer arenaViewer, GUI gui, PlayerController playerController, AlienController alienController, AmmoController ammoController) {
         super(arena);
         this.gui = gui;
         this.playerController = playerController;
         this.alienController = alienController;
+        this.ammoController = ammoController;
         this.arenaViewer = arenaViewer;
+        this.exit = false;
+    }
+
+    public boolean exit(){
+        return this.exit;
     }
 
     @Override
@@ -38,11 +48,13 @@ public class ArenaController extends Controller<Arena> {
         arenaViewer.draw();
         processAction(gui.getAction());
         alienController.step();
+        ammoController.step();
         checkAlienProjectilesCollisions();
     }
 
     public void processAction(GUI.Action action){
         switch (action) {
+            case EXIT -> this.exit = true;
             case KEYLEFT -> {
                 Position newPos = playerController.moveLeft();
                 if(checkLimits(newPos)) playerController.move(newPos);
