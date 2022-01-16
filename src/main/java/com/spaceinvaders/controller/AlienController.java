06b86bf087b372541b2d41d537c63999b0eab59e
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-16 00:46:12 +0000

    Implemented AlienController methods

--- src/main/java/com/spaceinvaders/controller/AlienController.java
+++ src/main/java/com/spaceinvaders/controller/AlienController.java
@@ -2,7 +2,9 @@ package com.spaceinvaders.controller;
 
 import com.spaceinvaders.model.Alien;
 import com.spaceinvaders.model.Arena;
+import com.spaceinvaders.model.Position;
 
+import java.util.List;
 
 public class AlienController extends Controller<Arena>{
     public AlienController(Arena model) {
@@ -11,10 +13,18 @@ public class AlienController extends Controller<Arena>{
 
     @Override
     public void step() {
-
+        for (List<Alien> list : getModel().getAliens()){
+            for (Alien alien : list){
+                move(alien);
+            }
+        }
     }
 
     private void move(Alien alien){
-
+        Position newPos = new Position(alien.getPosition().getX() + alien.getDirection(), alien.getPosition().getY());
+        if (newPos.getX() >= 0 && newPos.getX() <= getModel().getWidth() - 1)
+            alien.setPosition(newPos);
+        else
+            alien.changeDirection();
     }
 }
