a97f6c91757540a7acef77231c3d6d9bd8c671e8
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-18 23:46:18 +0000

    Added Button interface

---
+++ src/main/java/com/spaceinvaders/model/menu/Button.java
@@ -0,0 +1,31 @@
+package com.spaceinvaders.model.menu;
+
+import com.spaceinvaders.model.Position;
+
+public class Button {
+    private String text;
+    private String color;
+    private Position position;
+    private boolean highlight;
+
+    public Button(String text, String color, Position position){
+
+    }
+
+    public Position getPosition() {
+        return null;
+    }
+
+    public String getText() {
+        return null;
+    }
+
+    public String getColor() {
+        return null;
+    }
+
+    public boolean getHighlight(){
+        return false;
+    }
+}
+
