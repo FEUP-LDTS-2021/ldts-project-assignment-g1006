4653453870a540617b3c5757f62e3a3278e2ab06
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-18 13:07:31 +0000

    Updated GameState tests

---
+++ src/test/java/state/PlayingStateTest.java
@@ -0,0 +1,33 @@
+package state;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.Arena;
+import com.spaceinvaders.state.PlayingState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+public class PlayingStateTest {
+    private PlayingState state;
+    private Arena arena;
+    private GUI gui;
+
+    @BeforeEach
+    public void setup() throws IOException, URISyntaxException, FontFormatException {
+        this.arena = Mockito.mock(Arena.class);
+        this.gui = Mockito.mock(GUI.class);
+        this.state = new PlayingState(arena,gui);
+    }
+
+    @Test
+    void getController(){
+        Assertions.assertEquals(state.getController().getModel(), state.getModel());
+    }
+}
+
