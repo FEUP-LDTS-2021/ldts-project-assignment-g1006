22a5406cdcae85f7b342ea338640d88878927aab
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 22:58:31 +0000

    Added AddRecordCommand interface

---
+++ src/main/java/com/spaceinvaders/controller/command/AddRecordCommand.java
@@ -0,0 +1,14 @@
+package com.spaceinvaders.controller.command;
+
+import com.spaceinvaders.Game;
+
+public class AddRecordCommand extends ButtonCommand{
+    protected AddRecordCommand(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
