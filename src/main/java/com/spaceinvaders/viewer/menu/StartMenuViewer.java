e9d12c5bd5ade2eeb5c61de420d6dd02bb712832
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 15:39:55 +0000

    Added StartMenuViewer interface

---
+++ src/main/java/com/spaceinvaders/viewer/menu/StartMenuViewer.java
@@ -0,0 +1,18 @@
+package com.spaceinvaders.viewer.menu;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.menu.Menu;
+import com.spaceinvaders.viewer.Viewer;
+
+import java.io.IOException;
+
+public class StartMenuViewer extends Viewer<Menu> {
+    public StartMenuViewer(Menu model, GUI gui) {
+        super(model, gui);
+    }
+
+    @Override
+    public void draw() throws IOException {
+
+    }
+}
