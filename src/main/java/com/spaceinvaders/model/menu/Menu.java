1554bab74927bf7d64b0101580a0ff70ca9fb07c
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 14:53:49 +0000

    Updated Menu interface

--- src/main/java/com/spaceinvaders/model/menu/Menu.java
+++ src/main/java/com/spaceinvaders/model/menu/Menu.java
@@ -1,12 +1,43 @@
 package com.spaceinvaders.model.menu;
 
+import com.spaceinvaders.Game;
+
 import java.util.List;
 
-public class Menu {
+public abstract class Menu {
     protected List<Button> buttons;
     protected int selected;
+    protected Game game;
 
-    public Menu(){
+    public Menu(Game game){
+
+    }
+
+    public void setButtons(List<Button> buttons) {
+
+    }
+
+    public List<Button> getButtons() {
+        return null;
+    }
+
+    public Game getGame() {
+        return null;
+    }
+
+    public int getSelected() {
+        return -1;
+    }
+
+    public void setSelected(int selected) {
+        this.selected = selected;
+    }
+
+    public void nextButton(){
+
+    }
+
+    public void previousButton(){
 
     }
 }
