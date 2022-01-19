69f9aee7788dc3f1a9bc865d760f8475079d4195
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 23:52:24 +0000

    Added MainMenuCommand interface

---
+++ src/main/java/com/spaceinvaders/controller/command/MainMenuCommand.java
@@ -0,0 +1,14 @@
+package com.spaceinvaders.controller.command;
+
+import com.spaceinvaders.Game;
+
+public class MainMenuCommand extends ButtonCommand{
+    protected MainMenuCommand(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
