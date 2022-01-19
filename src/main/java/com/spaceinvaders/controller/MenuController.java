2fb3f0ee41163afe62538653f8c5d21a2d6b38c1
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 16:09:22 +0000

    Implemented MenuController methods

--- src/main/java/com/spaceinvaders/controller/MenuController.java
+++ src/main/java/com/spaceinvaders/controller/MenuController.java
@@ -16,6 +16,11 @@ public class MenuController extends Controller<Menu> {
 
     @Override
     public void step(Game game, long time) throws IOException {
-
+        GUI.Action action = gui.getAction();
+        switch (action){
+            case KEYDOWN -> getModel().nextButton();
+            case KEYUP -> getModel().previousButton();
+            case EXIT -> game.setGameState(null);
+        }
     }
 }
