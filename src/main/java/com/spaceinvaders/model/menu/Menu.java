cb02d149bb71a3a94071bd803d812d7d6ff5cb05
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-19 14:33:28 +0000

    Added Menu class

---
+++ src/main/java/com/spaceinvaders/model/menu/Menu.java
@@ -0,0 +1,12 @@
+package com.spaceinvaders.model.menu;
+
+import java.util.List;
+
+public class Menu {
+    protected List<Button> buttons;
+    protected int selected;
+
+    public Menu(){
+
+    }
+}
