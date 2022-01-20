3b200c519d6caa7716414634ae8b812a60381f09
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 19:44:00 +0000

    Added RecordsMenu interface

---
+++ src/main/java/com/spaceinvaders/model/menu/RecordsMenu.java
@@ -0,0 +1,9 @@
+package com.spaceinvaders.model.menu;
+
+import com.spaceinvaders.Game;
+
+public class RecordsMenu extends Menu{
+    public RecordsMenu(Game game) {
+        super(game);
+    }
+}
