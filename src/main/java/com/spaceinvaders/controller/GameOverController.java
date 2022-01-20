38e72c160b804cd482a8c960a56ff792675a8019
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 12:24:43 +0000

    Added GameOverController interface

---
+++ src/main/java/com/spaceinvaders/controller/GameOverController.java
@@ -0,0 +1,24 @@
+package com.spaceinvaders.controller;
+
+import com.spaceinvaders.Game;
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.menu.Menu;
+import com.spaceinvaders.viewer.menu.GameOverViewer;
+
+import java.io.IOException;
+
+public class GameOverController extends Controller<Menu>{
+    private final GameOverViewer viewer;
+    private final GUI gui;
+
+    public GameOverController(Menu model, GUI gui) {
+        super(model);
+        this.gui = gui;
+        this.viewer = new GameOverViewer(getModel(), gui);
+    }
+
+    @Override
+    public void step(Game game, long time) throws IOException {
+
+    }
+}
