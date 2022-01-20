ef1b3f2fc546ba5a24fdfc8747cc1bc3d5b8a16e
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 19:53:55 +0000

    Updated RecordsMenuController interface

--- src/main/java/com/spaceinvaders/controller/RecordsMenuController.java
+++ src/main/java/com/spaceinvaders/controller/RecordsMenuController.java
@@ -1,13 +1,20 @@
 package com.spaceinvaders.controller;
 
 import com.spaceinvaders.Game;
+import com.spaceinvaders.gui.GUI;
 import com.spaceinvaders.model.menu.Menu;
+import com.spaceinvaders.viewer.menu.RecordsMenuViewer;
 
 import java.io.IOException;
 
 public class RecordsMenuController extends Controller<Menu>{
-    public RecordsMenuController(Menu model) {
+    private RecordsMenuViewer viewer;
+    private GUI gui;
+
+    public RecordsMenuController(Menu model, GUI gui) {
         super(model);
+        this.gui = gui;
+        this.viewer = new RecordsMenuViewer(getModel(), gui);
     }
 
     @Override
