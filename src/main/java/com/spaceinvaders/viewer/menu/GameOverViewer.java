34829b5cf5347684c002c3f9a25b674ad4001ef3
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 12:17:23 +0000

    Implemented GameOverViewer methods

--- src/main/java/com/spaceinvaders/viewer/menu/GameOverViewer.java
+++ src/main/java/com/spaceinvaders/viewer/menu/GameOverViewer.java
@@ -1,6 +1,10 @@
 package com.spaceinvaders.viewer.menu;
 
 import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.Position;
+import com.spaceinvaders.model.menu.Button;
+import com.spaceinvaders.model.menu.GameOverMenu;
+import com.spaceinvaders.model.menu.GameWonMenu;
 import com.spaceinvaders.model.menu.Menu;
 import com.spaceinvaders.viewer.Viewer;
 
@@ -13,6 +17,14 @@ public class GameOverViewer extends Viewer<Menu> {
 
     @Override
     public void draw() throws IOException {
-
+        String timeStr = ((GameOverMenu)getModel()).time;
+        getGui().clear();
+        getGui().drawBackground();
+        getGui().drawText("GAME OVER", "#101010", new Position(17,5));
+        getGui().drawText("Time:", "#101010", new Position(18,8));
+        getGui().drawText(timeStr, "#101010", new Position(24,8));
+        for (Button button : getModel().getButtons())
+            getGui().drawButton(button);
+        getGui().refresh();
     }
 }
