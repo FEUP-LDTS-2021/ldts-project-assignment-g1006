16f4c19033afb3e7232bc439f751088389726830
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 12:29:10 +0000

    Added GameOverState tests

---
+++ src/test/java/com/spaceinvaders/state/GameOverStateTest.java
@@ -0,0 +1,28 @@
+package com.spaceinvaders.state;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.menu.GameOverMenu;
+import com.spaceinvaders.model.menu.GameWonMenu;
+import com.spaceinvaders.model.menu.Menu;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class GameOverStateTest {
+    private GameOverState state;
+    private Menu menu;
+    private GUI gui;
+
+    @BeforeEach
+    public void setup() {
+        this.menu = Mockito.mock(GameOverMenu.class);
+        this.gui = Mockito.mock(GUI.class);
+        this.state = new GameOverState(menu,gui);
+    }
+
+    @Test
+    void getController(){
+        Assertions.assertEquals(state.getController().getModel(), state.getModel());
+    }
+}
