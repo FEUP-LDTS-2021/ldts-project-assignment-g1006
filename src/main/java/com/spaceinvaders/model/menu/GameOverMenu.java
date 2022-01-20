d43981d0351b770aca0910146b4e005c915f60a0
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 12:06:44 +0000

    Added GameOverMenu interface

---
+++ src/main/java/com/spaceinvaders/model/menu/GameOverMenu.java
@@ -0,0 +1,10 @@
+package com.spaceinvaders.model.menu;
+
+import com.spaceinvaders.Game;
+
+public class GameOverMenu extends Menu{
+    public String time;
+    public GameOverMenu(Game game, long finalTime) {
+        super(game);
+    }
+}
