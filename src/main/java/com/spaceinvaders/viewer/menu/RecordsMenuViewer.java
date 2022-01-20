0035da19a313657580b27e695f283dfd1c484ac9
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 19:40:36 +0000

    Added RecordsMenuViewer interface

---
+++ src/main/java/com/spaceinvaders/viewer/menu/RecordsMenuViewer.java
@@ -0,0 +1,18 @@
+package com.spaceinvaders.viewer.menu;
+
+import com.spaceinvaders.gui.GUI;
+import com.spaceinvaders.model.menu.Menu;
+import com.spaceinvaders.viewer.Viewer;
+
+import java.io.IOException;
+
+public class RecordsMenuViewer extends Viewer<Menu> {
+    public RecordsMenuViewer(Menu model, GUI gui) {
+        super(model, gui);
+    }
+
+    @Override
+    public void draw() throws IOException {
+
+    }
+}
