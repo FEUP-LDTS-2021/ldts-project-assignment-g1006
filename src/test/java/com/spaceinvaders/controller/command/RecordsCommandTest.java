948b1771bd0a1bc50b0cd6d1bf0572c395114a4d
Alexandre Correia <correiaalexandrealex@gmail.com>
2022-01-20 20:03:02 +0000

    Added RecordsCommand test

--- src/test/java/com/spaceinvaders/controller/command/RecordsCommandTest.java
+++ src/test/java/com/spaceinvaders/controller/command/RecordsCommandTest.java
@@ -1,4 +1,24 @@
 package com.spaceinvaders.controller.command;
 
+import com.spaceinvaders.Game;
+import com.spaceinvaders.state.RecordsMenuState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class RecordsCommandTest {
+    private Game game;
+    private ButtonCommand buttonCommand;
+
+    @BeforeEach
+    void setup(){
+        this.game = Mockito.mock(Game.class);
+        this.buttonCommand = new RecordsCommand(game);
+    }
+
+    @Test
+    void play(){
+        buttonCommand.execute();
+        Mockito.verify(game, Mockito.times(1)).setGameState(Mockito.any(RecordsMenuState.class));
+    }
 }
